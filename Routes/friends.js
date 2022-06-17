7c3b3567a748d07367d9e3f3bbddb4d8d7436b5d
Ndeta Innocent <you@example.com>
2022-06-17 20:50:08 +0300

    Routing the controller

--- Routes/friends.js
+++ Routes/friends.js
@@ -1,6 +1,7 @@
 const {getFriend, getFriends,postFriend}=require('./../controllers/friends')
 const express=require('express')
 const friendsRouer=express.Router()
+//Routing the controller
 friendsRouer.get('/',getFriends).get('/:id', getFriend).post('/',postFriend)
 
 module.exports=friendsRouer
--- Routes/message.js
+++ Routes/message.js
@@ -1,5 +1,6 @@
 const {getMessage,getMessages,postMessages}=require('./../controllers/messages')
 const express=require('express')
 const messageRouter=express.Router()
+//Routing the controller
 messageRouter.get('/:id',getMessage).get('/',getMessages).post('/', postMessages)
 module.exports=messageRouter
